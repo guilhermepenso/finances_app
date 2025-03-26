898581f6f89252b72a79664dea9b8a7b3f0b0824
Guilherme Penso <guilhermepenso@gmail.com>
2025-03-26 20:16:15 -0300

    feat: propotype of resume.tsx component from home.tsx screen

--- components/ui/home/resume.tsx
+++ components/ui/home/resume.tsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
 import { Card, CardContent } from "~/components/ui/card";
 import { Text } from "../text";
 import { PieChart } from "react-native-gifted-charts";
 
 export const Resume: React.FC = () => {
-  const data = [{ value: 15 }, { value: 30 }, { value: 26 }, { value: 40 }];
+  const data = [
+    { type: "Entretenimento", value: 150.42 },
+    { type: "Estudos", value: 55.32 },
+    { type: "Boletos", value: 400.17 },
+    { type: "Empréstimos", value: 192.17 },
+  ];
+
+  useEffect(() => {}, [data]);
 
   return (
     <Card className="w-11/12">
-      <CardContent>
+      <CardContent className="native:p-1">
         <View className="flex flex-row items-center justify-between p-2">
           <Card>
-            <CardContent>
-              <Text>Item 1</Text>
-              <Text>Item 2</Text>
-              <Text>Item 3</Text>
+            <CardContent className="native:p-2 min-w-[65%]">
+              <View className="flex flex-col content-between justify-center">
+                {data.map((item: any) => (
+                  <View
+                    key={item.type}
+                    className="flex flex-row justify-between"
+                  >
+                    <Text>{item.type}</Text>
+                    <Text>R$ {item.value}</Text>
+                  </View>
+                ))}
+              </View>
             </CardContent>
           </Card>
-          <PieChart data={data}/>
+          <PieChart radius={50} data={data} />
         </View>
       </CardContent>
     </Card>
